d1d89bc955f84fd40c399d8416df4e3ab4566df5
KhangPham9 <khangpham9999@csu.fullerton.edu>
2022-05-15 20:11:22 -0700

    previous comments will now be loaded onto the website on start up

--- CLIENT/app/control/index.js
+++ CLIENT/app/control/index.js
@@ -1,31 +1,52 @@
 
+// getting the previously made comments
+const previousCommentXhr = new XMLHttpRequest();
+previousCommentXhr.open("GET", 'http://localhost:4020/api/commentsection');
+previousCommentXhr.responseType = 'json';
+previousCommentXhr.onreadystatechange = () => {
+	if(previousCommentXhr.readyState == 4 && previousCommentXhr.status == 200) {
+		let context = {option: previousCommentXhr.response.reverse()};
 
+		// get the template for the comments
+		const templateXhr = new XMLHttpRequest();
+		templateXhr.open("GET", "http://localhost:3000/prevComment.tbs");
+		templateXhr.responseType = "text";
+		templateXhr.onreadystatechange = () => {
+			if (templateXhr.status == 200 && templateXhr.readyState == 4) {
+				const template = Handlebars.compile(templateXhr.response);
+				const html = template(context);
 
+				// append template after the submit button
+				document.getElementById('comment-button-container').insertAdjacentHTML("afterend", html);
 
+			}
+		}
+		templateXhr.send();
+	}
+}
+previousCommentXhr.send();
 
-document.getElementById("login").addEventListener('click', (event)=>{
-	prompt();
-
-});
 
 
 function submitComment(){
 
+	// get the text from the textarea
 	let comments = document.getElementsByTagName('textarea');
-
 	let opt = {comment : comments[0].value};
 
 	let context = {opt};
 
+
+	// get the template
 	const xhr1 = new XMLHttpRequest();
 	xhr1.open("GET", "http://localhost:3000/comment.tbs");
 	xhr1.responseType = "text";
 	xhr1.onreadystatechange = () => {
 		if (xhr1.readyState == 4 && xhr1.status == 200) {
-			const button = document.getElementById('submit-button');
 			const template = Handlebars.compile(xhr1.response);
 			const html = template(context);
 
+			// append template after the submit button
 			document.getElementById('comment-button-container').insertAdjacentHTML("afterend", html);
 			comments[0].value = "";
 		}
@@ -34,6 +55,7 @@ function submitComment(){
 
 
 
+	// save the comment information into a mongodb database
 	const xhr2 = new XMLHttpRequest();
 	const jsonStr = JSON.stringify(opt);
 	console.log(jsonStr);
@@ -51,19 +73,5 @@ function submitComment(){
 
 }
 
-document.getElementById("submit-button").addEventListener('click', () => {
-	submitComment();
-});
-
-
-document.getElementById("test").addEventListener('click', () => {
-	const xhr = new XMLHttpRequest;
-	xhr.open("GET", 'http://localhost:4020/api/commentsection');
-	xhr.responseType = 'json';
-	xhr.onreadystatechange = () => {
-		if(xhr.readyState == 4 && xhr.status == 200) {
-			console.log(xhr.response);
-		}
-	}
-	xhr.send();
-});
+// adding event listener to the submit button
+document.getElementById("submit-button").addEventListener('click', submitComment);
